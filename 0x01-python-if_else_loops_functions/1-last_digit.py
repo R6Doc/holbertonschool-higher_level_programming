380fff19485ef4dc4fffe40428281156da2d4427
R6Doc <michaelgalindez274@gmail.com>
2020-04-28 15:17:50 -0500

    fixing logic in operations

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-        division = number % -10
+        result = number % -10
 else:
-        division = number % 10
-        print('Last digit of', number, 'is', division, end=' ')
-        if division > 5:
+        result = number % 10
+        print('Last digit of', number, 'is', result, end=' ')
+        if result > 5:
                 print('and is greater than 5')
-        elif division == 0:
+        elif result == 0:
                 print('and is 0')
         else:
                 print('and is less than 6 and not 0')
