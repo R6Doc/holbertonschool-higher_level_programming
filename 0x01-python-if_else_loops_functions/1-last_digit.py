d661ec92e9d6939ac2d547ef292645ddb2abfa5f
R6Doc <michaelgalindez274@gmail.com>
2020-04-28 15:23:30 -0500

    testing logic in vars

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-        result = number % -10
+            result = number % -10
 else:
         result = number % 10
         print('Last digit of', number, 'is', result, end=' ')
