bd8d1a0e521e7813cc95f78152e08b683860229f
R6Doc <michaelgalindez274@gmail.com>
2020-04-30 16:53:30 -0500

    fixing logic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    l = len(sys.argv)
-    print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments",
-                                 "." if l == 1 else ":"))
-    for i, s in enumerate(sys.argv):
-        if i > 0:
-            print("{:d}: {:s}".format(i, s))
+    l = len(sys.argv) - 1
+    if l == 1:
+        print("1 argument")
+    elif l == 0:
+        print("0 arguments")
+    else:
+        print("{} arguments:".format(l))
+    a = 0
+    for arg in sys.argv:
+        if a != 0:
+            print(a,": ", l)
+        a += 1
