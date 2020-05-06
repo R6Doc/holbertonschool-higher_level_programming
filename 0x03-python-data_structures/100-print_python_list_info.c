43a0f052c6bbcfc64db6fc954a044fc522c0bcb8
R6Doc <michaelgalindez274@gmail.com>
2020-05-05 23:23:05 -0500

    fixing logit

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,21 +2,21 @@
 
 void print_python_list_info(PyObject *p)
 {
-PyListObject *lista;
+PyListObject *list;
 Py_ssize_t size, i;
-PyObject *objecto;
+PyObject *object;
 struct _typeobject *type;
 
-if (strcmp(p->ob_type->tp_name, "lista") == 0)
+if (strcmp(p->ob_type->tp_name, "list") == 0)
 {
-lista = (PyListObject *)p;
-size = lista->ob_base.ob_size;
-printf("[*] Size of the Python Lista= %ld\n", size);
-printf("[*] Allocated = %ld\n", lista->allocated);
+list = (PyListObject *)p;
+size = list->ob_base.ob_size;
+printf("[*] Size of the Python List = %ld\n", size);
+printf("[*] Allocated = %ld\n", list->allocated);
 for (i = 0; i < size; i++)
 {
-objecto = lista->ob_item[i];
-type = objecto->ob_type;
+object = list->ob_item[i];
+type = object->ob_type;
 printf("Element %ld: %s\n", i, type->tp_name);
 }
 }
