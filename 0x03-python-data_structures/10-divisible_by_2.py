8d7ad2b4eae3ca8ed2ee768a9ca7487ce10fbf60
R6Doc <michaelgalindez274@gmail.com>
2020-05-05 20:23:12 -0500

    adding divisible logic

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    new = []
+    if my_list:
+        for elm in my_list:
+            new.append(False if elm % 2 else True)
+        return new
