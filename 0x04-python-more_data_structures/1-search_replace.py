f4a4bd85188ac76d4a2e34d8767dac6d692bd769
R6Doc <michaelgalindez274@gmail.com>
2020-05-07 15:22:20 -0500

    add 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    def search(elm):
-        return(elm if elm != search else replace)
-    return list(map(search, my_list))
+    return list(map(lambda elm: elm if elm != search else replace, my_list))
