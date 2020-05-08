ea96af2fd1be4b1b616ce6fa525d4abbf1377f5a
R6Doc <michaelgalindez274@gmail.com>
2020-05-07 20:43:20 -0500

    adding square matrix fix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-        return list(map((lambda elm: list(map((lambda x: x * x), elm))), matrix))
+    return list(map((lambda elm: list(map((lambda x: x * x), elm))), matrix))
