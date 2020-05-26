aa17224a101850c9ede7b51206af08b3fb76908c
R6Doc <michaelgalindez274@gmail.com>
2020-05-25 20:26:53 -0500

    adding rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
+"""
+Define a class Rectangle
+"""
+
+
 class Rectangle:
+    """represent a rectangle"""
     def __init__(self, width=0, height=0):
-        """Initialize triangle"""
+        """Initial rectangle"""
         self.height = height
         self.width = width
 
     @property
     def width(self):
-        """get private var"""
+        """get the private instance width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """set the private var width"""
+        """set the private instance width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -21,12 +27,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """get the private var height"""
+        """get the private instance height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """set the private attribute height"""
+        """set the private instance height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
