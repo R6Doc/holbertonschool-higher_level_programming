5827a19809fc8090b0ad2dd02df7e4b368f20885
R6Doc <michaelgalindez274@gmail.com>
2020-05-27 10:48:31 -0500

    fixing errors

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 class LockedClass:
         """A locked class that only let create the
-    attribute first_name"""
-            __slots__ = ['first_name']
+        attribute first_name"""
+        __slots__ = ['first_name']
