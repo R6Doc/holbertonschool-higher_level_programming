37170d3fea5a4b2115edb5236436ecc11d2bd1c2
R6Doc <fiendgear@gmail.com>
2020-06-02 19:06:33 -0500

    adding 2 and 3

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""
+module contains function is_same_class
+"""
+
+
 def is_same_class(obj, a_class):
+    """return true if objecto is class a_clas else false"""
+    return (type(obj) == a_class)
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""
+Contain is_kind_of_class function
+"""
+
+
 def is_kind_of_class(obj, a_class):
+    """True if objecto is an instancefrom a_class else False"""
+    return (isinstance(obj, a_class))
