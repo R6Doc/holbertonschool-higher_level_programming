458e16d19f8bff388ab271064b84efb56b7bbaed
R6Doc <fiendgear@gmail.com>
2020-06-02 19:09:18 -0500

    adding files

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""
+Contain inherits_from function
+"""
+
+
 def inherits_from(obj, a_class):
+    """return true if objecto is subclass of a_class else false"""
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)
--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+Contain class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """A class with public attribute"""
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
