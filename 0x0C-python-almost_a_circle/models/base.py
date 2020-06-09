aa1bb3eeaa89b73708a80136f187355b4bb4c133
R6Doc <fiendgear@gmail.com>
2020-06-09 12:45:44 -0500

    adding files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+Base class module
+"""
+
+
+class Base:
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Init Base class"""
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
+        else:
+            self.id = id
