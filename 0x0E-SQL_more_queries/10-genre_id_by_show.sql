0e064538fd9382a310bed98fd78bc0596bcec3ca
R6Doc <fiendgear@gmail.com>
2020-07-14 22:17:39 -0500

    finished files

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- shows all shows contained in hbtn_0d_tvshowswho have a genre linked.
+
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
