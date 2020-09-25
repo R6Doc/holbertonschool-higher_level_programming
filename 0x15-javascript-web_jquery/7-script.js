c892b3f5d6b5e31775ce632b74cafde63611bae5
R6Doc <fiendgear@gmail.com>
2020-09-24 23:38:53 -0500

    fixed semistandard

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+$(function () {
+  $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data, textStatus) {
+    if (textStatus === 'success') {
+      $('#character').text(data.name);
+    }
+  });
+});
--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+$(function () {
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data, textStatus) {
+    if (textStatus === 'success') {
+      let films = data.results;
+      for (let i in films) {
+        $('#list_movies').append('<li>' + films[i].title + '</li>');
+      }
+    }
+  });
+});
